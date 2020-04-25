d106dba1c7f21960c456b8d616a0994ccd0b6bbf
liyuhuan123 <2502268134@qq.com>
2020-04-25 20:18:29 +0800

    0425代码练习

---
+++ src/LearnJava_0425/Main.java
@@ -0,0 +1,13 @@
+package LearnJava_0425;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int N = sc.nextInt();
+//            System.out.println(Fibonacci(N));
+        }
+    }
+}
