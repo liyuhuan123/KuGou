6bd36b6931240209a7b00f1f8abf8e383104d7b1
liyuhuan123 <2502268134@qq.com>
2020-05-11 16:33:02 +0800

    0511代码练习

--- src/Learnjava_0511/Main.java
+++ src/Learnjava_0511/Main.java
@@ -3,26 +3,35 @@ package Learnjava_0511;
 import java.util.Scanner;
 
 public class Main {
-    //迷路的牛牛
+    //牛牛的闹钟
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] dir = new String[]{"N","E","S","W"};
         while(scanner.hasNext()){
-            int n = scanner.nextInt();
-            String str = scanner.next();
-            int count = 0;
-            for(int i = 0;i < n;i++){
-                if(str.charAt(i) == 'L'){
-                    count--;
+            int N = scanner.nextInt();
+            int hour = 0;
+            int minitue = 0;
+            int[] alarmtime = new int[N];
+            for(int i = 0;i < N;i++){
+                hour = scanner.nextInt();
+                minitue = scanner.nextInt();
+                alarmtime[i] = hour * 60 + minitue;
+            }
+            int spendtime = scanner.nextInt();
+            int arriveTime_hi = scanner.nextInt();
+            int arriveTime_mi = scanner.nextInt();
+            int arriveTime = arriveTime_hi * 60 + arriveTime_mi;
+            for(int i = 0;i < N;i++){
+                int result = arriveTime - spendtime - alarmtime[i];
+                if(result == 0){
+                    System.out.println((alarmtime[i] /60) + " " + (alarmtime[i] % 60));
+                    break;
+                }else if(result > 0){
+                    continue;
                 }else{
-                    count++;
+                    System.out.println((alarmtime[i - 1] / 60) + " " + (alarmtime[i - 1] % 60));
+                    break;
                 }
             }
-            count %= 4;
-            if(count < 0){
-                count += 4;
-            }
-            System.out.println(dir[count]);
         }
     }
 
