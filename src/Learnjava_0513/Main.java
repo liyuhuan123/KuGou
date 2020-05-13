8e88b4677aa0776dd489a5f88a389aa323da014d
liyuhuan123 <2502268134@qq.com>
2020-05-13 16:54:01 +0800

    0513代码练习

--- src/Learnjava_0513/Main.java
+++ src/Learnjava_0513/Main.java
@@ -1,24 +1,21 @@
 package Learnjava_0513;
 
 import java.util.Scanner;
-//字符串旋转
+//访友
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
-            String str = sc.next();
-            String[] arr = str.split(";");
-            if(arr[0].length() != arr[1].length()){
-                System.out.println("false");
-                return;
+            int x = sc.nextInt();
+            int[] walk = {1,2,3,4,5};
+            int sum = 0;
+            int y = 0;
+            for(int i = 4;i >= 0;i--){
+                y = x / walk[i];
+                x = x % walk[i];
+                sum += y;
             }
-            StringBuilder sb = new StringBuilder(arr[0]);
-            sb.append(arr[0]);
-            if(String.valueOf(sb).contains(arr[1])){
-                System.out.println("true");
-            }else{
-                System.out.println("false");
-            }
+            System.out.println(sum);
         }
     }
 }
