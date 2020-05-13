cfa418f6a3c796b807ecfdef3b7f80a2f74cb7c2
liyuhuan123 <2502268134@qq.com>
2020-05-13 18:11:32 +0800

    0513代码练习

--- src/Learnjava_0513/Main.java
+++ src/Learnjava_0513/Main.java
@@ -1,21 +1,26 @@
 package Learnjava_0513;
 
+import java.util.Arrays;
 import java.util.Scanner;
 //访友
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
-            int x = sc.nextInt();
-            int[] walk = {1,2,3,4,5};
-            int sum = 0;
-            int y = 0;
-            for(int i = 4;i >= 0;i--){
-                y = x / walk[i];
-                x = x % walk[i];
-                sum += y;
+            int[] arr = new int[3];
+            for(int i = 0;i < 3;i++){
+                arr[i] = sc.nextInt();
             }
-            System.out.println(sum);
+            for(int i = 0;i < arr.length;i++){
+                for(int j = i + 1;j < arr.length;j++){
+                    if(arr[i] < arr[j]){
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            System.out.println(arr[0] - arr[1] + arr[1] - arr[2]);
         }
     }
 }
