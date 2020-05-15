7b1e86349319c732babefcf6bfcf0fb657e9a96a
liyuhuan123 <2502268134@qq.com>
2020-05-15 22:30:29 +0800

    0515代码练习

--- src/Learnjava_0515/Main.java
+++ src/Learnjava_0515/Main.java
@@ -5,19 +5,46 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //万万没想到之聪明的编辑
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
-            int n = sc.nextInt();
-            String line;
-            int[] arr = new int[n];
-            for(int i = 0;i < n;i++){
-                line = sc.next();
-                 arr[i] = Integer.parseInt(line.substring(line.length() - 6,line.length()));
+            int N = sc.nextInt();
+            String str = sc.next();
+            String[] arr = new String[N];
+            for(int i = 0;i < N;i++){
+                str = sc.next();
+                arr[i] = editor(str);
             }
-            Arrays.sort(arr);
-            for(int i = 0;i < arr.length;i++){
+            for(int i = 0;i < N;i++) {
                 System.out.println(arr[i]);
-            }
             }
         }
     }
+    private static String editor(String str) {
+        if(str.length() > 2) {
+            for (int i = 0; i < str.length() - 3; i++) {
+                if (str.charAt(i) == str.charAt(i + 1) &&
+                        str.charAt(i) == str.charAt(i + 2)) {
+                    str = del(str, i);
+                    i--;
+                } else {
+                    if (str.charAt(i) == str.charAt(i + 1) &&
+                            str.charAt(i + 2) == str.charAt(i + 3)) {
+                        str = del(str, i + 2);
+                        i--;
+                    }
+                }
+            }
+        }
+        int n = str.length();
+        if(str.charAt(n - 1) == str.charAt(n - 2) &&
+        str.charAt(n - 1) == str.charAt(n - 3)){
+            str = del(str,n - 1);
+        }
+        return str;
+    }
+
+    private static String del(String str, int i) {
+        return str.substring(0,i) + str.substring(i + 1);
+    }
+}
