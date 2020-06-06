8ee280b512246b215677ca5581758a54cef68d08
liyuhuan123 <2502268134@qq.com>
2020-06-06 10:56:31 +0800

    0606代码练习

--- src/Learnjava_0606/Main.java
+++ src/Learnjava_0606/Main.java
@@ -1,23 +1,42 @@
 package Learnjava_0606;
 
-import java.util.Scanner;
+
+import java.util.*;
 
 public class Main {
-    //计算糖果
+    //n个数里出现次数大于等于n/2的数
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
-            int m = sc.nextInt();
-            int n = sc.nextInt();
-            int j = sc.nextInt();
-            int k = sc.nextInt();
-            int A = (m + j) / 2;
-            int B = (n + k) / 2;
-            int C = k - B;
-            if((m == A - B) && (n == B - C) && (j == A + B) && (k == B + C)){
-                System.out.println(A + " " + B + " " + C);
-            }else{
-                System.out.println("NO");
+            String line = sc.nextLine();
+            String[] arr = line.split(" ");
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0;i < arr.length;i++) {
+                list.add(Integer.parseInt(arr[i]));
+            }
+            int n = list.size() / 2;
+            Map<Integer,Integer> map = new HashMap<>();
+            for(int i = 0;i < list.size();i++){
+                map.put(list.get(i),map.getOrDefault(list.get(i),1) + 1);
+            }
+//            for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+//                int temp = map.get(entry);
+//                if(temp >= n){
+//                    System.out.println(temp);
+//                    break;
+//                }
+//            }
+            for(int i = 0;i < list.size();i++){
+                int flag = 0;
+                for(int j = 0;j < list.size();j++){
+                    if(list.get(i) == list.get(j)){
+                        flag++;
+                    }
+                }
+                if(flag >= list.size() / 2){
+                    System.out.println(list.get(i));
+                    break;
+                }
             }
         }
         sc.close();
