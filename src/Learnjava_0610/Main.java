0c9606d56c925319b86f941d759937b6bea1b756
liyuhuan123 <2502268134@qq.com>
2020-06-10 21:37:49 +0800

    0610代码练习

--- src/Learnjava_0610/Main.java
+++ src/Learnjava_0610/Main.java
@@ -1,6 +1,9 @@
 package Learnjava_0610;
 
+import java.util.Scanner;
 import java.util.Stack;
+
+import static java.lang.Math.abs;
 
 class TreeNode {
       int val;
@@ -9,68 +12,40 @@ class TreeNode {
       TreeNode(int x) { val = x; }
   }
 public class Main {
-    //修剪二叉搜索树
-    public TreeNode trimBST(TreeNode root, int L, int R) {
-        if(root == null){
-            return null;
-        }
-        if(root.val < L){
-            root = trimBST(root.right,L,R);
-        }else if(root.val > R){
-            root = trimBST(root.left,L,R);
-        }else{
-            root.left = trimBST(root.left,L,R);
-            root.right = trimBST(root.right,L,R);
-        }
-        return root;
-    }
-    //回文数
-    public boolean isPalindrome(int x) {
-        if(x < 0){
-            return false;
-        }
-        int y = x;
-        int result = 0;
-        while(x != 0){
-            int n = x %10;
-            result *= 10 + n;
-            n /= 10;
-        }
-        return result == y;
-    }
-    //栈的最小值
-    Stack<Integer> stack;
-    Stack<Integer> minstack;
-    int min = Integer.MAX_VALUE;
-    public void MinStack() {
-        stack = new Stack<>();
-        minstack = new Stack<>();
-    }
+    public static void main(String[] args) {
+        //Fiboncci数列
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+          int n = sc.nextInt();
+           System.out.println(step(n));
 
-    public void push(int x) {
-        stack.push(x);
-        if(x <= min){
-            minstack.push(x);
-            min = x;
         }
     }
 
-    public void pop() {
-        if(minstack.peek().equals(stack.pop())){
-            minstack.pop();
-            if(!minstack.empty()){
-                min = minstack.peek();
+    private static int step(int n) {
+        int num = 0,x = 0;
+        while(num >= 0){
+            if(Fibonacci(num) >= n){
+                x = Fibonacci(num);
+                break;
             }else{
-                min = Integer.MAX_VALUE;
+                num++;
             }
         }
+        int y = Fibonacci(num - 1);
+        return Math.min(abs(y - n),abs(x - n));
     }
 
-    public int top() {
-        return stack.empty() ? 0 : stack.peek();
-    }
 
-    public int getMin() {
-        return minstack.empty() ? 0 : minstack.peek();
+    private static int Fibonacci(int n) {
+        if(n == 0){
+            return 0;
+        }else{
+            if(n == 1){
+                return 1;
+            }else{
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
     }
 }
