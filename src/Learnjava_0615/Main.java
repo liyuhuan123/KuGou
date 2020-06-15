65d6921afe371f0bcdade7c20c758d96a56994d4
liyuhuan123 <2502268134@qq.com>
2020-06-15 21:00:54 +0800

    0615代码练习

--- src/Learnjava_0615/Main.java
+++ src/Learnjava_0615/Main.java
@@ -1,27 +1,69 @@
 package Learnjava_0615;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while(sc.hasNext()){
-            int n = sc.nextInt();
-            String c = sc.next();
-            for(int i = 1;i <= n;i++){
-                System.out.print(c);
-            }
-            System.out.println();
-            for(int i = 1;i <= Math.ceil((double) n / 2 - 2);i++){
-                System.out.print(c);
-                for(int j = 2;j <= n - 1;j++){
-                    System.out.print(" ");
-                }
-                System.out.println(c);
-            }
-            for(int i = 1;i <= n;i++){
-                System.out.print(c);
+//最长公共前缀
+public String longestCommonPrefix(String[] strs) {
+    if(strs.length == 0){
+        return "";
+    }
+    if(strs == null){
+        return null;
+    }
+    if(strs.length == 1){
+        return strs[0];
+    }
+    String str = strs[0];
+    for(int i = 1;i < strs.length;i++){
+        if(str.length() == 0 || strs[i].length() == 0){
+            return"";
+        }
+        int length = Math.min(str.length(),strs[i].length());
+        int j;
+        for(j =0;j < length;j++){
+            if(str.charAt(j) != strs[i].charAt(j)){
+                break;
             }
         }
+        str = str.substring(0,j);
+    }
+    return str;
+}
+//栈的压入、弹出序列
+public boolean validateStackSequences(int[] pushed, int[] popped) {
+    if(pushed.length == 0 && popped.length == 0){
+        return true;
+    }
+    if(pushed.length != popped.length){
+        return false;
+    }
+    Stack<Integer> stack = new Stack<>();//模拟压栈和入栈
+    int index = 0;//出栈位置的索引
+    for(int i = 0;i < pushed.length;i++){
+        stack.push(pushed[i]);
+        while(!stack.isEmpty() && stack.peek() == popped[index]){
+            stack.pop();
+            index++;
+        }
+    }
+    return stack.isEmpty();
+}
+//删除排序数组的重复项
+    public int removeDuplicates(int[] nums){
+        if(nums.length == 0 || nums == null){
+            return 0;
+        }
+        int count = 0;
+        for(int i = 1;i < nums.length;i++){
+            if(nums[count] != nums[i]){
+                count++;
+                nums[count] = nums[i];
+            }
+        }
+        return count + 1;
     }
 }
