103fdd765fc6f20d0d092ea85140e06fa51ded14
liyuhuan123 <2502268134@qq.com>
2020-06-16 12:40:03 +0800

    0616代码练习

--- src/Learnjava_0616/Main.java
+++ src/Learnjava_0616/Main.java
@@ -1,38 +1,34 @@
 package Learnjava_0616;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    //组个最小数
+    //尼科彻斯定理
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
-            int[] arr = new int[10];
-            for(int i = 0;i < 10;i++){
-                arr[i] = sc.nextInt();
-            }
-            List<Integer> list = new ArrayList<>();
-            if(arr[0] != 0){
-                for(int i = 1;i < 10;i++){
-                    if(arr[i] != 0){
-                        list.add(i);
-                        arr[i]--;
-                        break;
-                    }
-                }
-            }
-            for(int i = 0;i < 10;i++){
-                for(int j = 0;j < arr[i];j++){
-                    list.add(i);
-                }
-            }
-            for(int i : list){
-                System.out.print(i);
-            }
-
-
+            int m = sc.nextInt();
+            System.out.println(GetSequeOddNum(m));
         }
+    }
+
+    public static String GetSequeOddNum(int m) {
+        if(m == 1){
+            return String.valueOf(1);
+        }
+        String result = "";
+        int start = 0;
+        if(m % 2 != 0){
+            start = m * m - 2 * (m / 2);
+        }else{
+            start = m * m - 2 * (m / 2) + 1;
+        }
+        result += start;
+        for(int i = 1;i < m;i++){
+            ++start;
+            ++start;
+            result += "+" + start;
+        }
+        return result;
     }
 }
