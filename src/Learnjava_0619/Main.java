a52abf2a3a302d617f3f687c2d28d632679e681d
liyuhuan123 <2502268134@qq.com>
2020-06-19 11:30:48 +0800

    0619代码练习

--- src/Learnjava_0619/Main.java
+++ src/Learnjava_0619/Main.java
@@ -3,25 +3,53 @@ package Learnjava_0619;
 import java.util.Scanner;
 
 public class Main {
-    //统计同成绩学生人数
+    //洗牌
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNext()){
-            int N = sc.nextInt();
-            int[] score = new int[N];
-            while(N > 0){
-                score[N - 1] = sc.nextInt();
-                N--;
+        int T = sc.nextInt();//T组
+        while(T>0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] num1 = new int[n]; //接收前一半
+            int[] num2 = new int[n];//接收后半
+            for(int i = 0;i<n;i++){
+                num1[i] = sc.nextInt();
             }
-            int target = sc.nextInt();
-            int count = 0;
-            for(int x : score){
-                if(x == target){
-                    count++;
+            for(int i = 0;i<n;i++){
+                num2[i] = sc.nextInt();
+            }
+            int[] nums = new int[2*n];
+            for(int i = 0;i<k;i++){
+                fanZhuan(num1,num2,n,nums);
+            }
+            for(int i = 0;i<2*n;i++){
+                if(i == 2*n-1){
+                    System.out.print(nums[i]);
+                }else{
+                    System.out.print(nums[i]);
+                    System.out.print(" ");
                 }
             }
-            System.out.println(count);
+            T--;
+            if(T > 0){
+                System.out.println();
+            }
+        }
+
+    }
+    public static void fanZhuan(int[] num1,int[] num2,int n,int[] nums){
+        for(int i = 0;i<n;i++){
+            nums[2*i] = num1[i];
+        }
+        for(int i = 0;i<n;i++){
+            nums[2*i +1] = num2[i];
+        }
+        for(int i = 0;i<2*n;i++){
+            if(i<n){
+                num1[i] = nums[i];
+            }else{
+                num2[i-n] = nums[i];
+            }
         }
     }
-
 }
