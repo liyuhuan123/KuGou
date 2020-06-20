93cdc8c094f366cbb870e7daea88a209d1bd9b16
liyuhuan123 <2502268134@qq.com>
2020-06-20 21:40:40 +0800

    0620代码练习

--- src/Learnjava_0620/Main.java
+++ src/Learnjava_0620/Main.java
@@ -1,30 +1,13 @@
 package Learnjava_0620;
 
-import java.util.Scanner;
-
-//DNA序列
+//百万富翁问题
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while(sc.hasNext()){
-            String str = sc.nextLine();
-            int n = sc.nextInt();
-            int max = 0;
-            int index = 0;
-            for(int i = 0;i <= str.length() - n;i++){
-                int count  = 0;
-                for(int j = i;j < i + n;j++){
-                    if(str.charAt(j) == 'C' || str.charAt(j) == 'G'){
-                        count++;
-                    }
-                    if(count > max){
-                        max = count;
-                        index = i;
-                    }
-                }
-            }
-            System.out.println(str.substring(index,index + n));
-        }
+        int rich = 0;
+        int poor = 0;
+        rich = 10 * 30;
+        poor = (int)(Math.pow(2,30) - 1);
+        System.out.println(rich + " " + poor);
     }
 }
