0dd75282b9166c66b06e79a7e70cbe2835a345b2
liyuhuan123 <2502268134@qq.com>
2020-06-23 22:32:37 +0800

    0623代码练习

--- src/Learnjava_0623/main.java
+++ src/Learnjava_0623/main.java
@@ -3,12 +3,20 @@ package Learnjava_0623;
 import java.util.Scanner;
 
 public class main{
-    //统计每个月兔子的个数
+    //最难的问题
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
-            int monthCount = sc.nextInt();
-            System.out.println(getTotalCount(monthCount));
+            String str = sc.nextLine();
+            char[] array = str.toCharArray();
+            for(int i = 0;i < array.length;i++){
+                char temp = array[i];
+                if(temp >= 'A'){
+                    temp = (char) (('E' < temp) ? (temp - 5) : temp + 21);
+                    array[i] = temp;
+                }
+            }
+            System.out.println(new String(array));
         }
     }
     public static int getTotalCount(int monthCount){
