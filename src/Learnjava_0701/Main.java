29e45fa93b0a64ee0a1745b4fa5d06c8e0ccb8ac
liyuhuan123 <2502268134@qq.com>
2020-07-01 21:15:34 +0800

    0701代码练习

--- src/Learnjava_0701/Main.java
+++ src/Learnjava_0701/Main.java
@@ -4,28 +4,57 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //守形数
+        //密码验证合格程序
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNext()){
-            int N = sc.nextInt();
-            helper(N);
+        while (sc.hasNext()) {
+            String str = sc.nextLine();
+            helper(str);
         }
     }
-
-    private static void helper(int n) {
-        int temp = n * n;
-        if(n < 10){
-            if((temp - n) % 10 == 0){
-                System.out.println("Yes!");
+    private static void helper(String str){
+        int length = str.length();
+        if(length <= 8) {
+            System.out.println("NG");
+            return;
+        }
+        int[] arr = new int[4];
+        for(int i = 0;i < length;i++){
+            if(str.charAt(i) >= 48 && str.charAt(i) <= 57){
+                arr[0]++;
+            }else if(str.charAt(i) >= 65 && str.charAt(i) <= 90){
+                arr[1]++;
+            }else if(str.charAt(i) >= 97 && str.charAt(i) <= 122){
+                arr[2]++;
             }else{
-                System.out.println("No!");
-            }
-        }else{
-            if((temp - n) % 100 == 0){
-                System.out.println("Yes!");
-            }else{
-                System.out.println("No!");
+                arr[3]++;
             }
         }
+        int count = 0;
+        for(int i = 0;i < arr.length;i++){
+            if(arr[i] > 0){
+                count++;
+            }
+        }
+
+        if(count >= 3){
+            boolean temp = compare(str);
+            if(temp){
+                System.out.println("OK");
+            }else{
+                System.out.println("NG");
+            }
+        }else{
+            System.out.println("NG");
+        }
+    }
+    private static boolean compare(String str){
+        boolean flag = true;
+        for(int i = 0;i < str.length() - 2;i++){
+            String temp = str.substring(i,i + 3);
+            if(str.substring(i + 1).contains(temp)){
+                flag = false;
+            }
+        }
+        return flag;
     }
 }
