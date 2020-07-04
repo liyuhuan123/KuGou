475b9685dafb46a9a59957bdf49d9fd08b3c7d74
liyuhuan123 <2502268134@qq.com>
2020-07-04 20:31:09 +0800

    0704代码练习

--- src/Learnjava_0704/Main.java
+++ src/Learnjava_0704/Main.java
@@ -1,23 +1,38 @@
 package Learnjava_0704;
 
-import java.math.BigInteger;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //大整数排序
+        //奇偶校验
         while(sc.hasNext()){
-            int n = sc.nextInt();
-            BigInteger[] nums = new BigInteger[n];
-            for(int i = 0;i < n;i++){
-                nums[i] = sc.nextBigInteger();
+            String str = sc.nextLine();
+            helper(str.toCharArray());
+        }
+    }
+    private static void helper(char[] arr){
+        int[] result = new int[8];
+        for(int i = 0;i < arr.length;i++){
+            int n = 0x01;
+            int j = 7;
+            int sum = 0;
+            while(j > 0){
+                result[j] = (arr[i] & n) == 0 ? 0 : 1;
+                if(result[j] == 1){
+                    sum++;
+                }
+                n = n << 1;
+                j--;
             }
-            Arrays.sort(nums);
-            for(int i = 0;i < n;i++){
-                System.out.println(nums[i]);
+            if((sum & 1) == 0){
+                result[0] = 1;
             }
+            for(int k = 0;k < result.length;k++){
+                System.out.print(result[k]);
+            }
+            result[0] = 0;
+            System.out.println();
         }
     }
 }
