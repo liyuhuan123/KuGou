eb08473857f5527fd4db6b4856eb4d40ef1a76a7
liyuhuan123 <2502268134@qq.com>
2020-07-06 10:09:26 +0800

    0706代码练习

--- src/Learnjava_0706/Main.java
+++ src/Learnjava_0706/Main.java
@@ -1,19 +1,41 @@
 package Learnjava_0706;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        //说反话
+        //简单错误记录
         Scanner sc = new Scanner(System.in);
+        Map<String,Integer> map = new LinkedHashMap<>();
         while(sc.hasNext()){
             String str = sc.nextLine();
-            String[] arr = str.split(" ");
-            for(int i = arr.length - 1;i > 0;i--){
-                System.out.print(arr[i] + " ");
+            int index = str.lastIndexOf("\\");
+            if(index != 1){
+                str = str.substring(index + 1);
             }
-            System.out.print(arr[0]);
-            System.out.println();
+            if(!map.containsKey(str)){
+                map.put(str,map.get(str) + 1);
+            }
         }
+        Set<String> set = map.keySet();
+        int count = 0;
+        for(String temp : set){
+            count++;
+            if(count > (set.size()) - 8){
+                String[] s = temp.split(" ");
+                String name = s[0];
+                int len = name.length();
+                if(len > 16){
+                    name = name.substring(len - 16);
+                }
+                int line = Integer.parseInt(s[1]);
+                int number = map.get(temp);
+                System.out.println(name + " " + line + " " + number);
+            }
+        }
+
     }
 }
