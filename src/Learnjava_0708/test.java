bcf08a2098f035b5f5028b51090fdd9308f697dd
liyuhuan123 <2502268134@qq.com>
2020-07-08 22:11:18 +0800

    0708代码练习

--- src/Learnjava_0708/test.java
+++ src/Learnjava_0708/test.java
@@ -1,23 +1,29 @@
 package Learnjava_0708;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class test {
     public static void main(String[] args) {
-        //球的半径和体积
+        //Broken KeyBoard
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
-            double x1 = sc.nextDouble();
-            double y1 = sc.nextDouble();
-            double z1 = sc.nextDouble();
-            double x2 = sc.nextDouble();
-            double y2 = sc.nextDouble();
-            double z2 = sc.nextDouble();
+            String str1 = sc.nextLine().toUpperCase();
+            String str2 = sc.nextLine().toUpperCase();
 
-            double r = Math.sqrt((x1 -x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
-            System.out.printf("%.3f",r);
-            System.out.print(" ");
-            System.out.printf("%.3f",4 * Math.acos(-1) * r * r * r / 3);
+            Set<Character> result = new HashSet<>();
+            Set<Character> temp = new HashSet<>();
+            for(int i = 0;i < str2.length();i++){
+                result.add(str2.charAt(i));
+            }
+            for(int i = 0;i < str1.length();i++){
+                if(!result.contains(str1.charAt(i))){
+                    if(!temp.add(str1.charAt(i))){
+                        System.out.print(str1.charAt(i));
+                    }
+                }
+            }
         }
     }
 }
