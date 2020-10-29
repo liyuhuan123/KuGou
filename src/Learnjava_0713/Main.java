e00ac7948d0ea7c0a288f0c023c470d77d4d56d3
liyuhuan123 <2502268134@qq.com>
2020-10-29 19:40:55 +0800

    1029代码练习

--- src/Learnjava_0713/Main.java
+++ src/Learnjava_0713/Main.java
@@ -17,6 +17,7 @@ public class Main {
                     result+=" ";
                 }
             }
+            result.trim();
             return result;
         }
         public String reverse(String str){
