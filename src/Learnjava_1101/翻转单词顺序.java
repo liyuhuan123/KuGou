ea22b06ef9ff49e9d619779cf8f3d420637c51b7
liyuhuan123 <2502268134@qq.com>
2020-11-01 09:33:39 +0800

    1101剑指offer练习

---
+++ src/Learnjava_1101/翻转单词顺序.java
@@ -0,0 +1,16 @@
+package Learnjava_1101;
+//https://leetcode-cn.com/problems/fan-zhuan-dan-ci-shun-xu-lcof/
+public class 翻转单词顺序 {
+    public String reverseWords(String s) {
+        s = s.trim(); // 删除首尾空格
+        int j = s.length() - 1, i = j;
+        StringBuilder res = new StringBuilder();
+        while(i >= 0) {
+            while(i >= 0 && s.charAt(i) != ' ') i--; // 搜索首个空格
+            res.append(s.substring(i + 1, j + 1) + " "); // 添加单词
+            while(i >= 0 && s.charAt(i) == ' ') i--; // 跳过单词间空格
+            j = i; // j 指向下个单词的尾字符
+        }
+        return res.toString().trim(); // 转化为字符串并返回
+    }
+}
