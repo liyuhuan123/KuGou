a97b1712e8ecd004afa7282067b3a2a77b0f0e05
liyuhuan123 <2502268134@qq.com>
2020-11-08 09:58:44 +0800

    1108代码练习

---
+++ src/Learnjava_1108/员工的重要性.java
@@ -0,0 +1,29 @@
+package Learnjava_1108;
+
+import java.util.*;
+
+//链接：https://leetcode-cn.com/problems/employee-importance/
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
+public class 员工的重要性 {
+    public int getImportance(List<Employee> employees, int id) {
+        Map<Integer,Employee> map = new HashMap<>();
+        for(Employee employee : employees){
+            map.put(employee.id,employee);
+        }
+        Queue<Employee> queue = new LinkedList<>();
+        queue.offer(map.get(id));
+        int sum = 0;
+        while(!queue.isEmpty()){
+            Employee employee = queue.poll();
+            sum += employee.importance;
+            for(int subld : employee.subordinates){
+                queue.offer(map.get(subld));
+            }
+        }
+        return sum;
+    }
+}
