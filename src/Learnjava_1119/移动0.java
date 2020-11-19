bce9151cfeccc00f224cf30f0f87734452a96f52
liyuhuan123 <2502268134@qq.com>
2020-11-19 20:58:03 +0800

    1119代码练习

---
+++ src/Learnjava_1119/移动0.java
@@ -0,0 +1,20 @@
+package Learnjava_1119;
+//链接:https://leetcode-cn.com/problems/move-zeroes/
+public class 移动0 {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    public void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
