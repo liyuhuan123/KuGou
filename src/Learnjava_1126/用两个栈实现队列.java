05559bb7392345fed0e83ae394ee0244347a324f
liyuhuan123 <2502268134@qq.com>
2020-11-26 20:09:15 +0800

    1126代码练习

---
+++ src/Learnjava_1126/用两个栈实现队列.java
@@ -0,0 +1,21 @@
+package Learnjava_1126;
+
+import java.util.Stack;
+
+public class 用两个栈实现队列 {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+}
