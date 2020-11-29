6a3380939e94cb793554a85e6177885f92b81e8c
liyuhuan123 <2502268134@qq.com>
2020-11-29 11:45:35 +0800

    实现序列化和反序列化以及单元测试代码

---
+++ LearnServlet/src/test/java/util/JSONUtilTest.java
@@ -0,0 +1,32 @@
+package util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JSONUtilTest {
+    //单元测试:Junit框架使用方法上@Test注解,保证方法为public void
+    @Test
+    public void testSerialize(){
+        //测试序列化操作:使用map模拟复杂
+        Map map = new HashMap();
+        map.put("className","java");
+        map.put("students",new int[]{1,2});
+        String json = JSONUtil.serialize(map);
+        System.out.println(json);
+        Assert.assertNotNull(json);
+    }
+    @Test
+    public void testDeserialize(){
+        //测试反序列化操作
+        //类加载器加载某个资源,返回输入流
+        InputStream is = JSONUtilTest.class.getClassLoader().
+                getResourceAsStream("login.json");
+        Map map = JSONUtil.deserialize(is,Map.class);
+        System.out.println(map);
+        Assert.assertNotNull(map);
+    }
+}
