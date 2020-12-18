f1058a0812651d3fb06fed936beda091d3ca9cb1
liyuhuan123 <2502268134@qq.com>
2020-12-18 22:42:40 +0800

    找不同

---
+++ src/Learnjava_1218/找不同.java
@@ -0,0 +1,16 @@
+package Learnjava_1218;
+
+
+//https://leetcode-cn.com/problems/find-the-difference/
+public class 找不同{
+    public char findTheDifference(String s, String t) {
+        int length = s.length();
+        char temp = 0;
+        for(int i = 0;i < length;i++){
+        temp ^= s.charAt(i);
+        temp ^= t.charAt(i);
+        }
+        temp ^= t.charAt(t.length() - 1);
+        return temp;
+        }
+}
