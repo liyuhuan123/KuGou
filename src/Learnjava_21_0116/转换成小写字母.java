1c00fc7be8aa88ac6e651465a1de0e4aee17e4f7
liyuhuan123 <2502268134@qq.com>
2021-01-16 19:29:26 +0800

    转换成小写字母

---
+++ src/Learnjava_21_0116/转换成小写字母.java
@@ -0,0 +1,7 @@
+package Learnjava_21_0116;
+//链接:https://leetcode-cn.com/problems/to-lower-case/
+public class 转换成小写字母 {
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
