efb1babc9792c4a84e9a9e85b9d5c50e88bdce27
liyuhuan123 <2502268134@qq.com>
2021-03-21 18:28:13 +0800

    整数拆分

---
+++ src/Learnjava_21_0321/整数拆分.java
@@ -0,0 +1,16 @@
+package Learnjava_21_0321;
+
+public class 整数拆分 {
+    //动态规划:转移方程:dp[i] = Math.max(j * (i - j), j * dp[i - j]);
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            int curMax = 0;
+            for (int j = 1; j < i; j++) {
+                curMax = Math.max(curMax, Math.max(j * (i - j), j * dp[i - j]));
+            }
+            dp[i] = curMax;
+        }
+        return dp[n];
+    }
+}
